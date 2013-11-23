40a012a64701ca5151b207f6a5cbc283c48e4ac9
Sebastian <haloz@gmx.net>
2013-11-23 21:11:05 +0100

    add FizzBuzz kata

---
+++ FizzBuzz/src/Runner.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: intellibook
+ * Date: 23.11.13
+ * Time: 21:09
+ * To change this template use File | Settings | File Templates.
+ */
+public class Runner {
+}
