1f9c33618f27d8bb1d9481935bfde5b9417ea7d6
unknown <slange@SLANGE.intern.epages.de>
2013-12-27 13:47:12 +0100

    setup+teardown multiple movies

--- WestphalMovieRent/src/main/java/Customer.java
+++ WestphalMovieRent/src/main/java/Customer.java
@@ -2,11 +2,14 @@
  * Created by haloz on 27.12.13.
  */
 public class Customer {
+    private int totalCharge = 0;
+
     public void rentMovie(int days) {
+        totalCharge += 2;
 
     }
 
     public int getTotalCharge() {
-        return 2;
+        return totalCharge;
     }
 }
--- WestphalMovieRent/src/test/groovy/CustomerTest.groovy
+++ WestphalMovieRent/src/test/groovy/CustomerTest.groovy
@@ -1,22 +1,34 @@
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 
 /**
  * Created by haloz on 27.12.13.
  */
 class CustomerTest extends groovy.util.GroovyTestCase {
-    void setUp() {
-        super.setUp()
 
+    private Customer customer;
+
+    @Before
+    void setUp() {
+        customer = new Customer()
     }
 
+    @After
     void tearDown() {
-
+        customer = null
     }
 
     @Test
     public void testRentingOneMovie() {
-        Customer customer = new Customer()
         customer.rentMovie 1
         assertTrue customer.getTotalCharge() == 2
+    }
+
+    @Test
+    public void testRentingTwoMovies() {
+        customer.rentMovie 1
+        customer.rentMovie 2
+        assertEquals 4, customer.getTotalCharge()
     }
 }
